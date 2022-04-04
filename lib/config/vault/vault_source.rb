9b3bcb4673fecef49daf9594d779604d670e3696
David Young <da.young@f5.com>
2022-04-04 12:48:28 -0500

    Added automatic retry on vault ops

--- lib/config/vault/vault_source.rb
+++ lib/config/vault/vault_source.rb
@@ -13,10 +13,16 @@ module Config
       # @option opts [String, nil] :kv mount point for operations
       # @option opts [Array<String>, nil] :paths paths for vault secrets
       # @option opts [String, Symbol, nil] :root root key for data provided by source
+      # @option opts [Integer] :attempts number of attempts to try and resolve Vault::HTTPError
+      # @option opts [Number] :base interval for exponential backoff
+      # @option opts [Number] :max_wait maximum weight time for exponential backoff
       def initialize(opts = {})
         client_opts = opts.clone
         @kv = client_opts.delete(:kv) || ''
         @paths = client_opts.delete(:paths) || []
+        @attempts = client_opts.delete(:attempts) || 5
+        @base = client_opts.delete(:base) || 0.5
+        @max_wait = client_opts.delete(:max_wait) || 2.5
         @root = client_opts.delete(:root)
         @client = Vault::Client.new(client_opts)
       end
@@ -41,21 +47,23 @@ module Config
       #
       # @return [Hash]
       def load
-        process_paths
+        Vault.with_retries(Vault::HTTPError,
+                           attempts: @attempts,
+                           base: @base,
+                           max_wait: @max_wait) do
+          process_paths
+        end
       end
 
-      # Client reference for quick operations
-      #
-      # @return [Vault::KV, Vault:Logical]
-      def client
+      private
+
+      def client_ops
         unless kv.empty?
           @client.kv(@kv)
         else
           @client.logical
         end
       end
-
-      private
 
       def process_paths
         root = {}
@@ -75,13 +83,13 @@ module Config
           query_path, idx, parent = stack.pop
           sp = subpaths[idx]
           if sp.nil? || sp.eql?('*')
-            data = client.read(query_path)&.data
+            data = client_ops.read(query_path)&.data
             parent.merge!(data || {})
             parent.compact!
           end
 
           if sp.eql?('**') || sp.eql?('*')
-            subtrees = client.list(query_path)
+            subtrees = client_ops.list(query_path)
             subtrees.each do |st|
               new_parent = {}
               new_key = st.split('/').last.downcase.to_sym
